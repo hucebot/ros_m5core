fe2fd24208b3c1dfb8c269feedfdd9da556bd210
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2022-10-03 14:36:27 +0200

    working with button

--- main/main.cpp
+++ main/main.cpp
@@ -1,3 +1,8 @@
+#define CONFIG_MICRO_ROS_APP_STACK 4000
+#define CONFIG_MICRO_ROS_APP_TASK_PRIO 5
+#include <M5Core2.h>
+#include <WiFi.h>
+
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -14,24 +19,18 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){ printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); M5.Lcd.printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);M5.Lcd.printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
 rcl_publisher_t publisher;
 std_msgs__msg__Int32 msg;
 
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
-{
-	RCLC_UNUSED(last_call_time);
-	if (timer != NULL) {
-		printf("Publishing: %d\n", msg.data);
-		RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-		msg.data++;
-	}
-}
 
 void micro_ros_task(void * arg)
 {
+	printf("starting task...");
+	M5.Lcd.printf("Starting ROS2 task...\n");
+
 	rcl_allocator_t allocator = rcl_get_default_allocator();
 	rclc_support_t support;
 
@@ -40,6 +39,7 @@ void micro_ros_task(void * arg)
 
 	// create init_options
 	RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
+
 
 	// create node
 	rcl_node_t node;
@@ -50,26 +50,25 @@ void micro_ros_task(void * arg)
 		&publisher,
 		&node,
 		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-		"freertos_int32_publisher"));
+		"eurobin_iot/button_a"));
 
-	// create timer,
-	rcl_timer_t timer;
-	const unsigned int timer_timeout = 1000;
-	RCCHECK(rclc_timer_init_default(
-		&timer,
-		&support,
-		RCL_MS_TO_NS(timer_timeout),
-		timer_callback));
-
-	// create executor
-	rclc_executor_t executor;
-	RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-	RCCHECK(rclc_executor_add_timer(&executor, &timer));
+	printf("Publisher created\n");
 
 	msg.data = 0;
 
+	printf("Starting while loop...\n");
+	M5.Lcd.setTextColor(GREEN, BLACK);
+	M5.Lcd.printf("ROS2 Node ready\n");
+	M5.Lcd.setTextColor(WHITE, BLACK);
+	
 	while(1){
-		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+		//rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+		M5.update(); 
+		M5.Lcd.setCursor(0, 90);
+		M5.Lcd.printf("A: %d", M5.BtnA.read());
+		printf("Publishing: %d\n", msg.data);
+		msg.data = M5.BtnA.read();
+		RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
 		usleep(10000);
 	}
 
@@ -80,12 +79,25 @@ void micro_ros_task(void * arg)
   	vTaskDelete(NULL);
 }
 
+extern "C" {
 void app_main(void)
 {
+	M5.begin();
+    M5.Lcd.fillScreen(BLACK); // Set the screen
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.setTextSize(2);
+	M5.Lcd.setTextColor(BLUE);
+    M5.Lcd.printf("Eurobin IOT ROS2\n");
+	M5.Lcd.printf("SSID: %s\n", CONFIG_ESP_WIFI_SSID);
+    M5.Lcd.setTextColor(WHITE);
+
+    printf("Starting main...\n");
 #if defined(CONFIG_MICRO_ROS_ESP_NETIF_WLAN) || defined(CONFIG_MICRO_ROS_ESP_NETIF_ENET)
+    printf("checking network interface...\n");
     ESP_ERROR_CHECK(uros_network_interface_initialize());
 #endif
 
+    printf("Creating the task...\n");
     //pin micro-ros task in APP_CPU to make PRO_CPU to deal with wifi:
     xTaskCreate(micro_ros_task,
             "uros_task",
@@ -93,4 +105,6 @@ void app_main(void)
             NULL,
             CONFIG_MICRO_ROS_APP_TASK_PRIO,
             NULL);
+    printf("Task created\n");
+}
 }
